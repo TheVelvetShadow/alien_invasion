2666c9765efb17cb49490b43216ba18f45a4a482
TheVelvetShadow <matt@thevelvetshadow.com>
2024-11-19 20:01:42 +0000

    Initial commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings():
+    """ A class to store all settings for Alien Invasion. """
+
+    def __init__(self):
+        """ Initialise the game's settings. """
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230)
+
+        # Ship Settings
+        self.ship_speed_factor = 1.5
