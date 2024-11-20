8608d50aa6fcc98bf879edfb72691a3a5cd13110
TheVelvetShadow <matt@thevelvetshadow.com>
2024-11-20 10:40:28 +0000

    Added Bullet Firing
    bullet firing, limit no. of bullets, delete old bullets, bullet settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,27 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
 
 def run_game():
-    #Initialise game and create a screen object.
+    # Initialise game and create a screen object.
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #Make a ship.
+    # Make a ship.
     ship = Ship(ai_settings, screen)
-    
+    # Make a group to stores bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)        
+        gf.update_bullets(bullets) 
+        gf.update_screen(ai_settings, screen, ship, bullets)        
 
 run_game()                
--- settings.py
+++ settings.py
@@ -3,10 +3,17 @@ class Settings():
 
     def __init__(self):
         """ Initialise the game's settings. """
-        # Screen settings
+        # Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_colour = (230, 230, 230)
 
         # Ship Settings
         self.ship_speed_factor = 1.5
+
+        # Bullet Settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = 60, 60, 60
+        self.bullets_allowed = 3
